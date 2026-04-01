db92845d2c686780b0a7183c3c504807db6fcfd2
leonhnguyen <lleonhn@gmail.com>
2026-04-01 09:55:42 -0700

    negative test

--- tests/test_sleep_sort.py
+++ tests/test_sleep_sort.py
@@ -2,11 +2,16 @@ import pytest
 import src.sleep_sort as sleep_sort
 
 def test_is_sorted_simple():
-    sleep_sort.sort([5, 3, 1, 4, 2])
     assert sleep_sort.sort([5, 3, 1, 4, 2]) == [1, 2, 3, 4, 5]
     
 
 def test_pre_sorted():
-    sleep_sort.sort([5, 3, 1, 4, 2])
     assert sleep_sort.sort([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
     
+
+def test_neg_pre_sort():
+    assert sleep_sort.sort([-5,-4,-3,-2,-1]) == (-5,-4,-3,-2,-1)
+    
+    
+def test_neg_sort_simple():
+    assert sleep_sort.sort([-4,-1,-3,-5,-2]) == (-5,-4,-3,-2,-1)

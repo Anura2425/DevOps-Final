e53a05fc73f70888ad51cbfceeeb3463ed011bf1
leonhnguyen <lleonhn@gmail.com>
2026-04-14 13:05:19 -0700

    simple sleep sort for positive numbers

--- src/sleep_sort.py
+++ src/sleep_sort.py
@@ -1,4 +1,27 @@
+from asyncio import threads
+import threading
+import time
 
 def sort(list):
-    #TODO: implement sort
-    return list
+    newList = []
+    def sleep(n, maxValue):
+        time.sleep(n/maxValue)
+        newList.append(n)
+    
+    if not list:
+        return
+    
+    maxValue = max(list) or 1
+    threads = []
+    
+    for num in list:
+        thread = threading.Thread(target=sleep, args=(num, maxValue))
+        thread.start()
+        threads.append(thread)
+        
+    
+    for thread in threads:
+        thread.join()
+    
+        
+    return newList
